1de61936901de8dbb80f82e12048b5926128199c
Thomas Alane <thomhero10@gmail.com>
2025-05-21 08:11:50 -0300

    retirei o domcontentload pois nao era necessário

--- assets/js/tela-login.js
+++ assets/js/tela-login.js
@@ -1,15 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () { //função para garantir que o js só seja executado qdo o html tiver 100% carregado
-  const form = document.getElementById("forme");
-  const email = document.getElementById("email");
-  const senha = document.getElementById("senha");
+const form = document.getElementById("forme");
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
 
-  form.addEventListener("submit", function (event) {
-    event.preventDefault(); // impede o envio automatico do formulario
+form.addEventListener("submit", function (event) {
+event.preventDefault(); // impede o envio automatico do formulario
 
-    if (email.value && senha.value) {
-      window.location.href = "home.html"; 
-    } else {
-      alert("Todos os campos devem estar preenchidos para avançar");
+if (email.value && senha.value) {
+  window.location.href = "home.html"; 
+  } else {
+  alert("Todos os campos devem estar preenchidos para avançar");
     }
   });
-});
+
