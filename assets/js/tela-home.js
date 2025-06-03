ffc0c105bf42e12721570df3c819fa65a907c7f5
Thomas Alane <thomhero10@gmail.com>
2025-06-02 22:40:54 -0300

    fluxo tela de denuncia pronto

--- assets/js/tela-home.js
+++ assets/js/tela-home.js
@@ -1,6 +1,7 @@
 const apelido = localStorage.getItem('apelido')
 const apelido_lim = apelido.slice(0, 7); //corta o nome para 7 letras pq nao tem espaço no header
 const botao_pix = document.getElementById('botao-pix')
+const botao_denuncia = document.getElementsByClassName('denuncia')[0]
 
 document.getElementById('boas-vindas').textContent = `Olá, ${apelido_lim}`
 
@@ -12,4 +13,8 @@ botao_qrcode = document.getElementById('botao-qrcode')
 
 botao_qrcode.addEventListener('click', function() {
     window.location.href = '/assets/html/QR.html'
+})
+
+botao_denuncia.addEventListener('click', function() {
+    window.location.href = '/assets/html/pré-denucia.html'
 })
