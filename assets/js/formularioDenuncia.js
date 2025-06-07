02b03c4d4c5a0f39601c32907b238370dd79f433
Joao Pedro Dantas Thal <jpdantasthal@gmail.com>
2025-06-07 12:15:33 -0300

    formulario de denuncia testado

--- assets/js/formularioDenuncia.js
+++ assets/js/formularioDenuncia.js
@@ -7,3 +7,5 @@ voltar.addEventListener('click', function() {
 const apelido = localStorage.getItem('apelido')
 const apelido_lim = apelido.slice(0, 7); //corta o nome para 7 letras pq nao tem espaço no header
 document.getElementById('boas-vindas').textContent = `Olá, ${apelido_lim}`
+
+//testado
